137e60777f6deaeef89e53ca19a4aadcd1274df0
DanielOlvera <josue_olver@yahoo.com.mx>
2017-05-28 22:19:10 -0500

    Added Kotlin and libraries: butterknife and gson

---
+++ app/src/main/java/com/example/daniel/discoveritunes_02/view/SearchActivity.java
@@ -0,0 +1,15 @@
+package com.example.daniel.discoveritunes_02.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.daniel.discoveritunes_02.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
