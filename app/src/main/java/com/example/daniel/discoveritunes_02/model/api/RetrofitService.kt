666f0a300de714954ea827b15c9b6f7ef41eb624
DanielOlvera <josue_olver@yahoo.com.mx>
2017-05-29 22:50:12 -0500

    Finished refactor to use RxJava to manage the call

--- app/src/main/java/com/example/daniel/discoveritunes_02/model/api/RetrofitService.kt
+++ app/src/main/java/com/example/daniel/discoveritunes_02/model/api/RetrofitService.kt
@@ -4,19 +4,23 @@ import com.example.daniel.discoveritunes_02.BuildConfig
 import com.example.daniel.discoveritunes_02.model.ITunesResult
 import retrofit2.Call
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
+import rx.Observable
+import rx.Scheduler
+import rx.schedulers.Schedulers
 
 /**
  * Created by Daniel on 5/29/17.
  */
 interface RetrofitService {
     @GET("search")
-    fun getSearch(
+    fun search(
             @Query("term") term : String,
             @Query("entity") entity : String
-    ) : Call<ITunesResult>
+    ) : Observable<ITunesResult>
 
     /**
      * Companion object to create the service from Result class
@@ -25,6 +29,7 @@ interface RetrofitService {
         val BASE_URL = BuildConfig.BASE_URL
         fun create() : RetrofitService {
             val retrofit = Retrofit.Builder()
+                    .addCallAdapterFactory(RxJavaCallAdapterFactory.createWithScheduler(Schedulers.io()))
                     .addConverterFactory(GsonConverterFactory.create())
                     .baseUrl(BASE_URL)
                     .build()
@@ -33,3 +38,23 @@ interface RetrofitService {
         }
     }
 }
+
+/*
+Previous code for the call with only retrofit and gson:
+@GET("search")
+    fun getSearch(
+            @Query("term") term : String,
+            @Query("entity") entity : String
+    ) : Call<ITunesResult>
+
+val BASE_URL = BuildConfig.BASE_URL
+        fun create() : RetrofitService {
+            val retrofit = Retrofit.Builder()
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .baseUrl(BASE_URL)
+                    .build()
+
+            return retrofit.create(RetrofitService::class.java)
+        }
+
+ */
